58f99cdf1b2ff7cb20b8fa30755fa739ea46d613
Saidakbar <saidakbar_2000@mail.ru>
2022-05-30 14:14:56 +0500

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return(
+        <div className='flex'>
+            {/*<img src="" alt="HeaderLogo"/>*/}
+            logo
+            <div>
+                <p className=''>Home</p>
+                <p className=''>Why</p>
+                <p className=''>Features</p>
+                <p className=''>Screenshots</p>
+                <p className=''>Pricing</p>
+                <p className=''>Download</p>
+            </div>
+        </div>
+    )
+}
+export default Header
